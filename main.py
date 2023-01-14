345121f285340bf60784a55fdc6d9ea19bf4f6a5
hhhyyeee <changeling@gmail.com>
2023-01-14 17:18:54 +0900

    Step 7: Automating Backpropagation

--- main.py
+++ main.py
@@ -14,7 +14,19 @@ def f(x):
     C = Square()
     return C(B(A(x)))
 
-x = Variable(np.array(0.5))
-print(x.data)
-print(numerical_diff(f, x))
+
+if __name__ == '__main__':
+
+    A = Square()
+    B = Exp()
+    C = Square()
+
+    x = Variable(np.array(0.5))
+    a = A(x)
+    b = B(a)
+    y = C(b)
+
+    y.grad = np.array(1.0)
+    y.backward()
+    print(x.grad)
 
