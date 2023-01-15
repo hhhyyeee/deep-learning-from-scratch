b800d92cdc6bae9a7adf389ad460edbfd5c5ea6e
hhhyyeee <changeling@gmail.com>
2023-01-15 17:44:28 +0900

    Step 10: Test

--- main.py
+++ main.py
@@ -4,29 +4,25 @@ import numpy as np
 PROJECT_DIR = '/Users/hyewon/Documents/Projects/deep-learning-from-scratch/DeZero'
 sys.path.append(PROJECT_DIR)
 
-from modules.diff import *
+from modules.funcs import *
 from modules.utils import *
+from modules.test import *
 
 
-def f(x):
-    A = Square()
-    B = Exp()
-    C = Square()
-    return C(B(A(x)))
+def square(x):
+    return Square()(x)
+
+def exp(x):
+    return Exp()(x)
 
 
 if __name__ == '__main__':
 
-    A = Square()
-    B = Exp()
-    C = Square()
-
     x = Variable(np.array(0.5))
-    a = A(x)
-    b = B(a)
-    y = C(b)
+    print(type(x.data))
 
-    y.grad = np.array(1.0)
+    y = square(exp(square(x)))
+
     y.backward()
     print(x.grad)
 
--- modules/utils.py
+++ modules/utils.py
@@ -1,7 +1,12 @@
 import numpy as np
 
+
 class Variable:
     def __init__(self, data):
+        if data is not None:
+            if not isinstance(data, np.ndarray):
+                raise TypeError(f'{type(data)}is not supported')
+
         self.data = data
         self.grad = None
         self.creator = None
@@ -10,21 +15,30 @@ class Variable:
         self.creator = func
     
     def backward(self):
-        f = self.creator
-        if f is not None:
-            x = f.input
-            x.grad = f.backward(self.grad)
-            x.backward()
+        if self.grad is None:
+            self.grad = np.ones_like(self.data)
+        
+        funcs = [self.creator]
+        while funcs:
+            f = funcs.pop()
+            x, y = f.input, f.output
+            x.grad = f.backward(y.grad)
+
+            if x.creator is not None:
+                funcs.append(x.creator)
 
 
 class Function:
     def __call__(self, input):
         x = input.data
         y = self.forward(x)
-        output = Variable(y)
+
+        output = Variable(as_array(y))
         output.set_creator(self)
+
         self.input = input
         self.output = output
+
         return output
 
     def forward(self, x):
@@ -34,22 +48,8 @@ class Function:
         raise NotImplementedError
 
 
-class Square(Function):
-    def forward(self, x):
-        return x ** 2
-    
-    def backward(self, gy):
-        x = self.input.data
-        gx = 2 * x * gy
-        return gx
-
-
-class Exp(Function):
-    def forward(self, x):
-        return np.exp(x)
-
-    def backward(self, gy):
-        x = self.input.data
-        gx = np.exp(x) * gy
-        return gx
+def as_array(x):
+    if np.isscalar(x):
+        return np.array(x)
+    return x
 
