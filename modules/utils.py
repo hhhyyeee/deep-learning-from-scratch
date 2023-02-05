e1b40af649123c5c7dd0bec8bb283f28ad2fadb4
hhhyyeee <changeling@gmail.com>
2023-02-05 14:24:20 +0900

    Step 19: Make It Easier To Use The Variables

--- modules/utils.py
+++ modules/utils.py
@@ -5,18 +5,43 @@ import numpy as np
 
 
 class Variable:
-    def __init__(self, data):
+    def __init__(self, data, name=None):
         if data is not None:
             if not isinstance(data, np.ndarray):
                 raise TypeError(f'{type(data)}is not supported')
 
         self.data = data
+        self.name = name
         self.grad = None
         self.creator = None
         self.generation = 0
 
         self.count = 0
     
+    @property
+    def shape(self):
+        return self.data.shape
+    
+    @property
+    def ndim(self):
+        return self.data.ndim
+    
+    @property
+    def size(self):
+        return self.data.size
+    
+    @property
+    def dtype(self):
+        return self.data.dtype
+    
+    def __len__(self):
+        return len(self.data)
+    
+    def __repr__(self):
+        if self.data is None:
+            return 'variable(None)'
+        p = str(self.data).replace('\n', '\n'+' '*9)
+
     def set_creator(self, func):
         self.creator = func
         self.generation = func.generation + 1
@@ -89,6 +114,8 @@ class Function:
 
         def no_grad():
             return using_config('enable_backprop', False)
+        
+        no_grad()
 
         if Config.enable_backprop:
             self.generation = max([x.generation for x in inputs])
