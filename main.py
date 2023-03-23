f26e02758567b71242d4b8e1d781cbf1764a3788
hhhyyeee <changeling@gmail.com>
2023-03-23 15:12:54 +0900

    Step 45: Layers containing Layers

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ if '__file__' in globals():
 
 import numpy as np
 import pandas as pd
-from dezero import Variable
+from dezero import Variable, Model
 import dezero.functions as F
 import dezero.layers as L
 import matplotlib.pyplot as plt
@@ -25,33 +25,37 @@ if __name__ == '__main__':
     # df.plot.scatter('x', 'y')
     # plt.show()
 
-    l1 = L.Linear(10)
-    l2 = L.Linear(1)
-
-    def predict(x):
-        y = l1(x)
-        y = F.sigmoid(y)
-        y = l2(y)
-        return y
-    
     lr = 0.2
     iters = 10000
+    hidden_size = 10
+
+    class TwoLayerNet(Model):
+        def __init__(self, hidden_size, out_size):
+            super().__init__()
+            self.l1 = L.Linear(hidden_size)
+            self.l2 = L.Linear(out_size)
+        
+        def forward(self, x):
+            y = self.l1(x)
+            y = F.sigmoid(y)
+            y = self.l2(y)
+            return y
+    
+    model = TwoLayerNet(hidden_size, 1)
 
     for i in range(iters):
-        y_pred = predict(x)
+        y_pred = model(x)
         loss = F.mean_squared_error(y, y_pred)
 
-        l1.cleargrads()
-        l2.cleargrads()
+        model.cleargrads()
         loss.backward()
 
-        for l in [l1, l2]:
-            for p in l.params():
-                p.data -= lr * p.grad.data
+        for p in model.params():
+            p.data -= lr * p.grad.data
         if i % 1000 == 0:
             print(loss)
 
-    final_pred = predict(x)
+    final_pred = model(x)
 
     df = pd.DataFrame({
         'x': x.reshape(100,).tolist(), 'y': y.reshape(100,).tolist()
